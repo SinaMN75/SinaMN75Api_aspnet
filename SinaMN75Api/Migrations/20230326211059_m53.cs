12090eb5b995ee65b073600160a57db8e4bd42dd
Mohamadhosein <mhroohiamini@gmail.com>
2023-03-27 01:02:29 +0330

    add seen groupChat

---
+++ SinaMN75Api/Migrations/20230326211059_m53.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SinaMN75Api.Migrations
+{
+    public partial class m53 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UsersSeen",
+                table: "GroupChatMessage",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UsersSeen",
+                table: "GroupChatMessage");
+        }
+    }
+}
