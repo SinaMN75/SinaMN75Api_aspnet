dd39b35e02359e8ed790247ca95825aae91eda3f
Sina MohammadZadeh <34570827+SinaMN75@users.noreply.github.com>
2023-04-08 21:27:46 +0330

    :)

---
+++ SinaMN75Api/Migrations/20230408175706_m65.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SinaMN75Api.Migrations
+{
+    public partial class m65 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "ParentId",
+                table: "Bookmarks",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bookmarks_ParentId",
+                table: "Bookmarks",
+                column: "ParentId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Bookmarks_Bookmarks_ParentId",
+                table: "Bookmarks",
+                column: "ParentId",
+                principalTable: "Bookmarks",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Bookmarks_Bookmarks_ParentId",
+                table: "Bookmarks");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Bookmarks_ParentId",
+                table: "Bookmarks");
+
+            migrationBuilder.DropColumn(
+                name: "ParentId",
+                table: "Bookmarks");
+        }
+    }
+}
