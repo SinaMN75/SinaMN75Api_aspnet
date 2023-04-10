f91bc82fd48268a730a860b73e04db60ab53a5b1
Sina MohammadZadeh <34570827+SinaMN75@users.noreply.github.com>
2023-04-10 10:57:49 +0330

    :)

---
+++ SinaMN75Api/Migrations/20230410072729_m67.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SinaMN75Api.Migrations
+{
+    public partial class m67 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UseCase",
+                table: "GroupChat");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Type",
+                table: "GroupChat",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(500)",
+                oldMaxLength: 500,
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Type",
+                table: "GroupChat",
+                type: "nvarchar(500)",
+                maxLength: 500,
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UseCase",
+                table: "GroupChat",
+                type: "nvarchar(500)",
+                maxLength: 500,
+                nullable: true);
+        }
+    }
+}
