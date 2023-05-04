6650c3f5f925b07a641b7e5f4ab24c153bd5f013
Mohamadhosein <mhroohiamini@gmail.com>
2023-05-04 15:06:32 +0330

    add signalR for test

--- SinaMN75Api/Program.cs
+++ SinaMN75Api/Program.cs
@@ -1,14 +1,20 @@
 using SinaMN75Api.Core;
+using SinaMN75Api.Hubs;
 using Utilities_aspnet.Hubs;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 builder.SetupUtilities<AppDbContext>(builder.Configuration.GetConnectionString("ServerSQLServer"));
 
+builder.Services.AddSignalR();
+
 WebApplication app = builder.Build();
 
 app.UseUtilitiesServices();
 
 app.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
+
+app.MapHub<MoveViewHub>("/movehub");
+
 
 app.Run();
