7e5a53aa25e07ea28d523534abf02d94f0f5a1b3
Mohamadhosein <mhroohiamini@gmail.com>
2023-05-16 10:29:56 +0330

    add-migration 83 for expireupgradeacoount

---
+++ SinaMN75Api/Migrations/20230516065241_m83.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SinaMN75Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class m83 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ExpireUpgradeAccount",
+                table: "AspNetUsers",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ExpireUpgradeAccount",
+                table: "AspNetUsers");
+        }
+    }
+}
