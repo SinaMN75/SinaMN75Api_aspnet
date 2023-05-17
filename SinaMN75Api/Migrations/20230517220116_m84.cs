c4f853c56128251199d3152b02a8ddbb76f81f5b
Mohamadhosein <mhroohiamini@gmail.com>
2023-05-18 01:36:15 +0330

    add-migration

---
+++ SinaMN75Api/Migrations/20230517220116_m84.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SinaMN75Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class m84 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "RecieverPhoneNumber",
+                table: "Address",
+                newName: "ReceiverPhoneNumber");
+
+            migrationBuilder.RenameColumn(
+                name: "RecieverFullName",
+                table: "Address",
+                newName: "ReceiverFullName");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ReceiverPhoneNumber",
+                table: "Address",
+                newName: "RecieverPhoneNumber");
+
+            migrationBuilder.RenameColumn(
+                name: "ReceiverFullName",
+                table: "Address",
+                newName: "RecieverFullName");
+        }
+    }
+}
