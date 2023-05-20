4195b8fca4e8c4ea5683192c753e9b689c49e125
Mohamadhosein <mhroohiamini@gmail.com>
2023-05-20 13:09:04 +0330

    add-migration authorize done

---
+++ SinaMN75Api/Migrations/20230520093511_m88.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SinaMN75Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class m88 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsAuthorize",
+                table: "AspNetUsers",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsForeigner",
+                table: "AspNetUsers",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsAuthorize",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "IsForeigner",
+                table: "AspNetUsers");
+        }
+    }
+}
