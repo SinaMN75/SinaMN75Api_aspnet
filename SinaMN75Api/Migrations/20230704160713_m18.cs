75891205c9dac3a37df8c388d4f7d16392d81dfd
Sina MohammadZadeh <34570827+SinaMN75@users.noreply.github.com>
2023-07-04 19:37:35 +0330

    :)

---
+++ SinaMN75Api/Migrations/20230704160713_m18.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SinaMN75Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class m18 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Type",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "UseCase",
+                table: "AspNetUsers");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Tags",
+                table: "Notifications",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Tags",
+                table: "Notifications");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Type",
+                table: "AspNetUsers",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UseCase",
+                table: "AspNetUsers",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: true);
+        }
+    }
+}
