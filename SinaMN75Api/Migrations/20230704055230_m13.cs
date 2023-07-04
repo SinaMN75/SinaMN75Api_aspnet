598a5020050eee3e464039ffd93aa0ea69a7d2e9
Mohamadhosein <mhroohiamini@gmail.com>
2023-07-04 09:22:55 +0330

    گزینه فیلتر بر اساس شخصی بودن یوزر در پروداکت

---
+++ SinaMN75Api/Migrations/20230704055230_m13.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SinaMN75Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class m13 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsPrivate",
+                table: "AspNetUsers",
+                type: "bit",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(bool),
+                oldType: "bit",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsPrivate",
+                table: "AspNetUsers",
+                type: "bit",
+                nullable: true,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+        }
+    }
+}
