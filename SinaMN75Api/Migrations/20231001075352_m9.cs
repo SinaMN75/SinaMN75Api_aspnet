5c759c0e2ece6b2980f868f6fde1b43c12bd1dc4
Sina MohammadZadeh <34570827+SinaMN75@users.noreply.github.com>
2023-10-01 11:25:44 +0330

    :(

---
+++ SinaMN75Api/Migrations/20231001075352_m9.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SinaMN75Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class m9 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "StatusId",
+                table: "Transactions");
+
+            migrationBuilder.DropColumn(
+                name: "TransactionType",
+                table: "Transactions");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Tags",
+                table: "Transactions",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Tags",
+                table: "Transactions");
+
+            migrationBuilder.AddColumn<int>(
+                name: "StatusId",
+                table: "Transactions",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "TransactionType",
+                table: "Transactions",
+                type: "int",
+                nullable: true);
+        }
+    }
+}
