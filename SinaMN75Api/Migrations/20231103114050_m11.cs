283beceea2febc7ce2ae8f806766c941e71570ca
Sina MohammadZadeh <34570827+SinaMN75@users.noreply.github.com>
2023-11-03 15:11:49 +0330

    :)

---
+++ SinaMN75Api/Migrations/20231103114050_m11.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SinaMN75Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class m11 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UseCase",
+                table: "Media");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FileName",
+                table: "Media",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50,
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "FileName",
+                table: "Media",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100,
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UseCase",
+                table: "Media",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: true);
+        }
+    }
+}
