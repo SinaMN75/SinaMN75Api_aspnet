ec960ce33b80dd3de3f71dfd32b74b1692673b5b
Mohamadhosein <mhroohiamini@gmail.com>
2023-11-04 20:08:12 +0330

    update migration m12

---
+++ SinaMN75Api/Migrations/20231104163534_m12.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SinaMN75Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class m12 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "GroupChatMessageId",
+                table: "Products",
+                type: "uniqueidentifier",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "GroupChatMessageId",
+                table: "Products");
+        }
+    }
+}
