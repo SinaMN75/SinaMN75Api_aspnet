fd16378463fb6175072d19eee1d9af139d122144
Sina MohammadZadeh <34570827+SinaMN75@users.noreply.github.com>
2023-12-03 19:00:52 +0330

    :)

---
+++ SinaMN75Api/Migrations/20231203153020_m27.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SinaMN75Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class m27 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "Latitude",
+                table: "Products",
+                type: "float",
+                nullable: true);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Longitude",
+                table: "Products",
+                type: "float",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Latitude",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "Longitude",
+                table: "Products");
+        }
+    }
+}
