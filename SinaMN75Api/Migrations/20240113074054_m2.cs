77ebbababf1b2d8e407159810c5651bef2969d81
Sina MohammadZadeh <34570827+SinaMN75@users.noreply.github.com>
2024-01-14 12:08:21 +0330

    :)

---
+++ SinaMN75Api/Migrations/20240113074054_m2.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SinaMN75Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class m2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Transactions_Users_UserId",
+                table: "Transactions");
+
+            migrationBuilder.RenameColumn(
+                name: "UserId",
+                table: "Transactions",
+                newName: "SellerId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Transactions_UserId",
+                table: "Transactions",
+                newName: "IX_Transactions_SellerId");
+
+            migrationBuilder.AddColumn<string>(
+                name: "BuyerId",
+                table: "Transactions",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Code",
+                table: "Transactions",
+                type: "character varying(10)",
+                maxLength: 10,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Code",
+                table: "Products",
+                type: "character varying(10)",
+                maxLength: 10,
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transactions_BuyerId",
+                table: "Transactions",
+                column: "BuyerId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Transactions_Users_BuyerId",
+                table: "Transactions",
+                column: "BuyerId",
+                principalTable: "Users",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Transactions_Users_SellerId",
+                table: "Transactions",
+                column: "SellerId",
+                principalTable: "Users",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Transactions_Users_BuyerId",
+                table: "Transactions");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Transactions_Users_SellerId",
+                table: "Transactions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Transactions_BuyerId",
+                table: "Transactions");
+
+            migrationBuilder.DropColumn(
+                name: "BuyerId",
+                table: "Transactions");
+
+            migrationBuilder.DropColumn(
+                name: "Code",
+                table: "Transactions");
+
+            migrationBuilder.DropColumn(
+                name: "Code",
+                table: "Products");
+
+            migrationBuilder.RenameColumn(
+                name: "SellerId",
+                table: "Transactions",
+                newName: "UserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Transactions_SellerId",
+                table: "Transactions",
+                newName: "IX_Transactions_UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Transactions_Users_UserId",
+                table: "Transactions",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id");
+        }
+    }
+}
