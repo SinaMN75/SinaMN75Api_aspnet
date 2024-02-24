85384415ea63f9be6e1f7565baea61c8c47519bb
Sina MohammadZadeh <34570827+SinaMN75@users.noreply.github.com>
2024-02-24 15:36:26 +0330

    :)

---
+++ SinaMN75Api/Migrations/20240224120554_m3.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SinaMN75Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class m3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "ParentId",
+                table: "Media",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Media_ParentId",
+                table: "Media",
+                column: "ParentId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Media_Media_ParentId",
+                table: "Media",
+                column: "ParentId",
+                principalTable: "Media",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Media_Media_ParentId",
+                table: "Media");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Media_ParentId",
+                table: "Media");
+
+            migrationBuilder.DropColumn(
+                name: "ParentId",
+                table: "Media");
+        }
+    }
+}
